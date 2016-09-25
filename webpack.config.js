f78e91b418341f58b7c335c5adba723bd02b5774
Ham Chao <pallop.chao@gmail.com>
2016-09-26 00:53:22 +0700

    Setup babel & eslint, Create first component

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,21 @@
 module.exports = {
-  entry: "./entry.js",
+  entry: './entry.jsx',
   output: {
     path: __dirname,
-    filename: "bundle.js"
+    filename: 'bundle.js',
   },
   module: {
     loaders: [
-      { test: /\.css$/, loader: "style!css" }
-    ]
-  }
+      { test: /\.css$/, loader: 'style!css' },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'stage-0', 'react'],
+          plugins: ['transform-runtime'],
+        },
+      },
+    ],
+  },
 };
